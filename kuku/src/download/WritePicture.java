0a4a6d7047c8687cf981af4ec63b4480ee88725e
unknown <kshu@KSHU.adobenet.global.adobe.com>
2013-03-20 14:17:26 +0800

    jars

--- kuku/src/download/WritePicture.java
+++ kuku/src/download/WritePicture.java
@@ -26,6 +26,7 @@ public class WritePicture {
 
 	public WritePicture(String savePath) {
 		this.pictureSavePath = this.makeDirctory(savePath);
+		System.out.println(this.pictureSavePath);
 	}
 
 	private String makeDirctory(String path) {
